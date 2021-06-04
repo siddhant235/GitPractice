b91a839eeb44fcf3c92853941e1a494dd36515bc
siddhant235 <siddhant235@gmail.com>
2021-06-04 16:50:09 +0530

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,58 @@
+export const robots = [
+  {
+    id: '1',
+    name: '1 ',
+    username: '',
+    email: ''
+  },
+  {
+    id: '2',
+    name: '2',
+    username: '',
+    email: ''
+  },
+  {
+    id: '3',
+    name: '3',
+    username: '',
+    email: ''
+  },
+  {
+    id: '4',
+    name: '4',
+    username: 'KT',
+    email: ''
+  },
+  {
+    id: '5',
+    name: '5',
+    username: '',
+    email: ''
+  },
+  {
+    id: '6',
+    name: '6',
+    username: '',
+    email: '',
+   
+  },
+  {
+    id: 'shimbhat',
+    name: '7',
+    username: '',
+    email: '',
+    
+  },
+  {
+    id: '6',
+    name: '8',
+    username: '',
+    email: '',
+  },
+  {
+    id: '7',
+    name: '9',
+    username: '',
+    email: 'Working On It!'
+  }
+];
